92db9389328541aeb69cb777b28e64099f4661f8
dannydarku <dannydarku@gmail.com>
2019-04-09 04:38:05 -0400

    line before rating star

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,10 @@
 class Movie < ApplicationRecord
   belongs_to :user
+  has_many :reviews
   
   has_one_attached :image
+  
+  
   
   #has_attached_file :image
   #validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_09_064131) do
+ActiveRecord::Schema.define(version: 2019_04_09_080838) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -58,6 +58,14 @@ ActiveRecord::Schema.define(version: 2019_04_09_064131) do
     t.string "image_content_type"
     t.bigint "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "movie_id"
   end
 
   create_table "users", force: :cascade do |t|
